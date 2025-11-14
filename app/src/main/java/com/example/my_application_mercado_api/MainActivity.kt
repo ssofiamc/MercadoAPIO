0c157ee25b2f8ca2573fbacad8f9596935be70a9
ssofiamc <159634938+ssofiamc@users.noreply.github.com>
2025-11-14 18:05:51 -0500

    1

--- app/src/main/java/com/example/my_application_mercado_api/MainActivity.kt
+++ app/src/main/java/com/example/my_application_mercado_api/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     My_Application_Mercado_apiTheme {
-        Greeting("Android")
+        Greeting("puto")
     }
 }
