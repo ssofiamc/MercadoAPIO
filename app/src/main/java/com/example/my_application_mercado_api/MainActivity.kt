9e7c83417df9ea9c5997c6f6326bddc2d420fb9a
DavidSantiAmaya <160082902+DavidSantiAmaya@users.noreply.github.com>
2025-11-14 18:10:20 -0500

    4

--- app/src/main/java/com/example/my_application_mercado_api/MainActivity.kt
+++ app/src/main/java/com/example/my_application_mercado_api/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     My_Application_Mercado_apiTheme {
-        Greeting("flor")
+        Greeting("flor_3_perro")
     }
 }
