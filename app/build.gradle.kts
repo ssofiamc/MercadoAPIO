88680c70eb8005b55c7121d2f376c81306e1b2a4
DavidSantiAmaya <160082902+DavidSantiAmaya@users.noreply.github.com>
2025-11-14 18:37:48 -0500

    ser

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,9 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
 }
---
+++ app/src/main/java/com/example/my_application_mercado_api/services/models/Users.kt
@@ -0,0 +1,9 @@
+package com.example.my_application_mercado_api.services.models
+
+data class Users(
+
+    var id: Int,
+    var username : String,
+    var email : String,
+    var password:  String
+)
